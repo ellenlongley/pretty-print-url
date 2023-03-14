c6667882e37e822babbb4ad918e2f3b16b34ae32
Ellen Longley <ellenharperlongley@gmail.com>
2023-03-14 12:23:00 -0400

    added frontend back

---
+++ frontend/src/components/searchBox.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchBox = () => {
+  // You need a text input field and a submit button
+  // The text input when it changes need to save its data to a local state (using setState())
+  // The submit button will grab the data from the text input
+  // Add your own instructions and stuff
+  return (
+    <div>
+      <h1>Search</h1>
+    </div>
+  );
+};
+
+export default SearchBox;
