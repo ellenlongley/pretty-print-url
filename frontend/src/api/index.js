daf20999185688de306518b1893e07d176d948e4
Ellen Longley <ellenharperlongley@gmail.com>
2023-03-16 12:52:35 -0400

    adding docker

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL = "https://cyderes-project-backend.onrender.com";
 
 export const fetchWhoIsData = (domainName) => {
   const url = `${BASE_URL}?domainName=${domainName}`;
